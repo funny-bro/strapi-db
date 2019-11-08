c868fabeadb4112af25344ba58e56b680242e4d1
peter chang <wahengchang@gmail.com>
2019-11-08 18:02:51 +0800

    added customized api route

---
+++ api/view/controllers/View.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Urlcontent.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Urlcontent`.
+ */
+
+module.exports = {
+  index: async (ctx) => {
+    ctx.send('Hello World!');
+  }
+};
